204cd2f0c678559b3946bea450c655d23fea8f1e
RistalTV <ristalvtgames@yandex.com>
2019-12-22 15:15:50 +0300

    Delete output.h

--- output.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void output_str_1(int loc,char cat1[20],char cat2[20]);
-void output_str_2(char number[2],int loc1,char name[25],char cost[2]);
-char output_str_3(void);
-char input_str_2(void);
